a68db7a2904c51b2aed2c7eab461afce4ecd0465
David <davidrlutrick@gmail.com>
2022-07-22 21:30:03 -0700

    added bootstrap and boiler plate code to Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Tinky</h1>
+      <Navbar />
     </div>
   );
 }
